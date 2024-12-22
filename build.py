20c2cd868a09a379e0ac8f70eadd7e5df4a0d6aa
Bruno Cardoso <bc13092002@gmail.com>
2024-12-22 15:01:26 -0300

    Build automation development

--- build.py
+++ build.py
@@ -7,12 +7,14 @@ icon = os.path.abspath("./resources/icons/icon.ico")
 compile_with_nuitka(
     pyfile=pyfile,
     product_name="Simulador Circuitos Digitais",
-    output_filename="Simulador CD",
-    file_version="0.1.0",
-    file_description="Um simulador dos sistemas para implementação de circuitos digitais",
+    output_filename="simulador",
+    file_version="0.0.1.0",
+    file_description="Simulador Circuitos Digitais",
     copyright="Copyright 2024 Bruno Cardoso",
     icon=icon,
     disable_console=True,
-    onefile=True,
+    onefile=False,
     other_options=["--mingw64", "--enable-plugin=pyside6"]
 )
+
+#--include-qt-plugins=qml

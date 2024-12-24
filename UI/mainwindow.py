5097a1038d2f618dab5de0de7932d054eb5b41a0
Bruno Cardoso <bc13092002@gmail.com>
2024-12-24 14:42:32 -0300

    Added dark mode detection

--- UI/mainwindow.py
+++ UI/mainwindow.py
@@ -1,29 +1,32 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
-from PySide6.QtCore import Qt, Slot
-from PySide6.QtGui import QIcon
-
 from PySide6 import QtWidgets
+from PySide6.QtCore import Qt, Slot, QSettings
+from PySide6.QtGui import QIcon
+from style.style import globalStyle
 
 import resources.resources
 
 # Creating the main window
-class MainWindow(QMainWindow):
-    def __init__(self):
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, isDarkMode):
         super().__init__()
-        
+
+        self.settings = QSettings("BrunoCardoso", "SimuladorCircuitosDigitais")
+
+        self.isDarkMode = True if (self.settings.value("darkMode",defaultValue=isDarkMode, type=bool)) else False
+
         # Defining window parameters
         self.setWindowTitle("Simulador Circuitos Digitais")
         self.setMinimumWidth(800)
         self.setMinimumHeight(500)
         self.setWindowIcon(QIcon(":/images/icons/icon.ico"))
 
-        self.setStyleSheet("#CentralWidget{background-color: #181818;}")
+        self.setStyleSheet(globalStyle(self.isDarkMode))
 
-        self.central = QWidget(self)
+        self.central = QtWidgets.QWidget(self)
         self.central.setObjectName("CentralWidget")
         self.central.setAttribute(Qt.WA_StyledBackground, True)
 
-        self.layoutCentral = QHBoxLayout(self.central)
+        self.layoutCentral = QtWidgets.QHBoxLayout(self.central)
         self.layoutCentral.setContentsMargins(0, 0, 0, 0)
         self.layoutCentral.setSpacing(0)
 
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys
-from PySide6 import QtWidgets
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QStyleFactory
+from PySide6.QtGui import QIcon, QPalette
 
 from ui.mainwindow import MainWindow
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
+    app = QApplication([])
+    app.setStyle(QStyleFactory.create("Fusion"))
     app.setWindowIcon(QIcon(":/images/icons/icon.ico"))
 
+    windowColor = app.palette().color(QPalette.Window)
+    isDarkMode = False if windowColor.lightnessF() > 0.5 else True
+
     # Instantiating the main window
-    window = MainWindow()
+    window = MainWindow(isDarkMode=isDarkMode)
     # Setting the initial window size
     window.resize(800, 500)
     # Showing the main window

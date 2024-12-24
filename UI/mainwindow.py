52232c8f459ac135c1a4901495e117df650bdd56
Bruno Cardoso <bc13092002@gmail.com>
2024-12-24 14:10:14 -0300

    Creating the main window

--- UI/mainwindow.py
+++ UI/mainwindow.py
@@ -1,15 +1,37 @@
-from PySide6 import QtWidgets
+from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
+from PySide6.QtCore import Qt, Slot
 from PySide6.QtGui import QIcon
+
+from PySide6 import QtWidgets
 
 import resources.resources
 
-# Criando a janela principal
-class MainWindow(QtWidgets.QMainWindow):
+# Creating the main window
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        # Definindo os parametros da janela
+        # Defining window parameters
         self.setWindowTitle("Simulador Circuitos Digitais")
         self.setMinimumWidth(800)
         self.setMinimumHeight(500)
         self.setWindowIcon(QIcon(":/images/icons/icon.ico"))
+
+        self.setStyleSheet("#CentralWidget{background-color: #181818;}")
+
+        self.central = QWidget(self)
+        self.central.setObjectName("CentralWidget")
+        self.central.setAttribute(Qt.WA_StyledBackground, True)
+
+        self.layoutCentral = QHBoxLayout(self.central)
+        self.layoutCentral.setContentsMargins(0, 0, 0, 0)
+        self.layoutCentral.setSpacing(0)
+
+        self.central.setLayout(self.layoutCentral)
+
+        self.setCentralWidget(self.central)
+
+    @Slot()
+    def closeEvent(self, event):
+        QtWidgets.QApplication.quit()
+        super().closeEvent(event)
--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import sys
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6 import QtWidgets
 from PySide6.QtGui import QIcon
 
-from UI.mainwindow import MainWindow
-
-current_path = sys.argv[0].replace("main.py", "")
-
+from ui.mainwindow import MainWindow
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
-
     app.setWindowIcon(QIcon(":/images/icons/icon.ico"))
 
-    # Instanciando a janela principal
-    widget = MainWindow()
-    # Definindo o tamanho inicial da janela
-    widget.resize(800, 500)
-    # Mostrando a janela principal
-    widget.show()
+    # Instantiating the main window
+    window = MainWindow()
+    # Setting the initial window size
+    window.resize(800, 500)
+    # Showing the main window
+    window.show()
 
     sys.exit(app.exec())

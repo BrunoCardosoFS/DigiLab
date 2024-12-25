da92470349bdff485c1499095af2b33916e6663b
Bruno Cardoso <bc13092002@gmail.com>
2024-12-25 15:05:06 -0300

    Build

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ compile_with_nuitka(
     pyfile=pyfile,
     product_name="Simulador Circuitos Digitais",
     output_filename="simulador",
-    file_version="0.1.0",
+    file_version="0.0.1.0",
     file_description="Simulador Circuitos Digitais",
     copyright="Copyright 2024 Bruno Cardoso",
     icon=icon,

fb412041bd07ede91dd0a8d2fa729e51f4e20559
Bruno Cardoso <bc13092002@gmail.com>
2025-01-05 12:10:37 -0300

    COPYING

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#   Copyright (C) 2024 by Bruno Cardoso <contato@brunocardosofm.com.br>
+#   Copyright (C) 2025 by Bruno Cardoso <brunocardosofs@gmail.com>
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by

2f4cdcfa01ae1f80a93c93cc749b614d603411ea
Bruno Cardoso <bc13092002@gmail.com>
2025-01-05 18:12:37 -0300

    Creation of the class where the simulation will be carried out

--- ui/widgets/simulation.py
+++ ui/widgets/simulation.py
@@ -1,8 +1,29 @@
 from PySide6 import QtWidgets
+from PySide6 import QtCore
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtCore import Qt, Slot, QUrl
 
 import os
+
+class Simulation(QtWidgets.QWidget):
+    testeSignal = QtCore.Signal(dict)
+
+    def __init__(self, parent: QtWidgets.QWidget):
+        super().__init__(parent)
+
+        self.setMinimumWidth(50)
+        self.setMinimumHeight(50)
+        self.setAttribute(Qt.WA_StyledBackground, True)
+        self.setStyleSheet("background-color: #f00;")
+        self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+
+        self.Layout = QtWidgets.QVBoxLayout(self)
+    
+    @QtCore.Slot()
+
+    def testeSlot(self, lista: dict):
+        print(lista)
+
 
 class AreaSimulation(QtWidgets.QWidget):
     def __init__(self, parent: QtWidgets.QWidget):
@@ -28,6 +49,15 @@ class AreaSimulation(QtWidgets.QWidget):
         self.layoutScrollAreaWidget.setContentsMargins(3,3,3,3)
 
         self.scrollArea.setWidget(self.scrollAreaWidget)
+
+        self.spacer = QtWidgets.QSpacerItem(10, 10, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+
+        self.layoutScrollAreaWidget.addItem(self.spacer)
+
+        self.simulation = Simulation(self.scrollAreaWidget)
+        self.layoutScrollAreaWidget.addWidget(self.simulation)
+
+        self.layoutScrollAreaWidget.addItem(self.spacer)
 
         self.Layout.addWidget(self.scrollArea)
         

efdb122dd6e6274dc336bcadd7d879943453f753
Bruno Cardoso <bc13092002@gmail.com>
2025-01-05 23:37:44 -0300

    Adding features to leftMenu

--- ui/widgets/leftmenu.py
+++ ui/widgets/leftmenu.py
@@ -1,16 +1,61 @@
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
 from PySide6.QtCore import Qt, Slot
 
 class LeftMenu(QtWidgets.QWidget):
     def __init__(self, parent: QtWidgets.QWidget):
         super().__init__()
         
-        self.setObjectName("LeftMenu")
         self.setAttribute(Qt.WA_StyledBackground, True)
-
-        self.Layout = QtWidgets.QVBoxLayout(self)
-        self.setLayout(self.Layout)
-
         self.setFixedWidth(220)
         self.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding)
+
+        self.setObjectName("LeftMenu")
+
+        # Layout
+        self.Layout = QtWidgets.QVBoxLayout(self)
+        self.Layout.setContentsMargins(15, 10, 15, 10)
+
+        self.setLayout(self.Layout)
+
+        # Spacers
+        self.spacerExpanding = QtWidgets.QSpacerItem(10, 10, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.spacer = QtWidgets.QSpacerItem(10, 30, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+
+        # Title
+        self.SimulationTitle = QtWidgets.QLabel(text="Simulação", parent=self, alignment=QtCore.Qt.AlignCenter)
+
+        # Experiment selection
+        self.selectSimulation = QtWidgets.QComboBox(self)
+        self.selectSimulation.setPlaceholderText("Selecionar experimento")
+        self.selectSimulation.addItem("Tanque")
+        self.selectSimulation.addItem("Semáforo")
+        self.selectSimulation.addItem("Esteira")
+
+        # Device selection
+        self.selectDevice = QtWidgets.QComboBox(self)
+        self.selectDevice.setPlaceholderText("Selecionar dispositivo")
+
+        # Simulation controls
+        self.SimulationControls = QtWidgets.QWidget(self)
+        self.SimulationControls.setObjectName("SimulationControls")
+        self.SimulationControlsLayout = QtWidgets.QHBoxLayout(self.SimulationControls)
+        self.SimulationControlsLayout.setContentsMargins(0, 0, 0, 0)
+        self.SimulationControlsLayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
+
+        self.btnPlay = QtWidgets.QPushButton(parent=self.SimulationControls, icon=QtGui.QIcon(":/images/icons/play.svg"), text="")
+        self.btnPlay.setFixedSize(30, 30)
+
+        self.btnStop = QtWidgets.QPushButton(parent=self.SimulationControls, icon=QtGui.QIcon(":/images/icons/stop.svg"), text="")
+        self.btnStop.setFixedSize(30, 30)
+
+        self.SimulationControlsLayout.addWidget(self.btnPlay)
+        self.SimulationControlsLayout.addWidget(self.btnStop)
+
+
+        # Adding widgets and items to layout
+        self.Layout.addWidget(self.SimulationTitle)
+        self.Layout.addWidget(self.selectSimulation)
+        self.Layout.addWidget(self.selectDevice)
+        self.Layout.addWidget(self.SimulationControls)
+        self.Layout.addItem(self.spacerExpanding)
         

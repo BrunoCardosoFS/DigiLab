0aa9ff41c40c31b96d37017529dc066192126b5d
Bruno Cardoso <bc13092002@gmail.com>
2025-01-09 11:48:40 -0300

    Added device connection

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -1,6 +1,8 @@
 from PySide6 import QtWidgets
 from PySide6.QtCore import Qt, Slot, QSettings
 from PySide6.QtGui import QIcon
+from PySide6.QtSerialPort import QSerialPort
+
 from style.style import globalStyle
 
 from modules.tempsettings import TempSettings
@@ -32,16 +34,68 @@ class CentralWidget(QtWidgets.QWidget):
         self.LeftMenu = LeftMenu(self)
         self.LeftMenu.toggleDarkMode.connect(self.toggleTheme)
 
+        self.LeftMenu.selectDevice.currentIndexChanged.connect(self.connectSerial)
+        self.LeftMenu.closeSerial.connect(self.closeSerial)
+
         self.AreaSimulation = AreaSimulation(self)
 
         self.Layout.addWidget(self.LeftMenu)
         self.Layout.addWidget(self.AreaSimulation)
 
-    @Slot()
+        # Serial Manager
+        self.serialPort = QSerialPort(parent=self)
 
+    @Slot(bool)
     def toggleTheme(self, isDarkMode):
         self.isDarkMode = isDarkMode
         self.setStyleSheet(globalStyle(isDarkMode))
+
+    @Slot(str)
+    def connectSerial(self, index:int):
+        if index >= 0:
+            port = TempSettings.get("devices")[index][0]
+
+            TempSettings.set("currentDevice", port)
+
+            self.serialPort.setPortName(port)
+            self.serialPort.setBaudRate(QSerialPort.Baud115200)
+
+            # self.serialPort.setDataBits(QSerialPort.Data8)
+            # self.serialPort.setParity(QSerialPort.NoParity)
+            # self.serialPort.setStopBits(QSerialPort.OneStop)
+            # self.serialPort.setFlowControl(QSerialPort.NoFlowControl)
+
+            messageBox = QtWidgets.QMessageBox()
+
+            if self.serialPort.open(QSerialPort.ReadWrite):
+                messageBox.setWindowTitle(f"Conectado a {port}")
+                messageBox.setIcon(QtWidgets.QMessageBox.Information)
+                messageBox.setText("Conexão estabelecida com sucesso.")
+                messageBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+
+                messageBox.exec()
+            else:
+                self.LeftMenu.updateDevices(True)
+                messageBox.setWindowTitle("Não foi possível conectar")
+                messageBox.setIcon(QtWidgets.QMessageBox.Critical)
+                messageBox.setText("Não foi possível conectar ao dispositivo.")
+                messageBox.setInformativeText("Verifique o dispositivo e tente novamente.")
+                messageBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+
+                messageBox.exec()
+
+    @Slot()
+    def closeSerial(self):
+        if self.serialPort.isOpen():
+            self.serialPort.close()
+            messageBox = QtWidgets.QMessageBox()
+            messageBox.setWindowTitle(f"Desconectado de {TempSettings.get('currentDevice')}")
+            messageBox.setIcon(QtWidgets.QMessageBox.Warning)
+            messageBox.setText("Conexão encerrada, escolha um dispositivo para conectar.")
+            messageBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+
+            messageBox.exec()
+
 
 # Creating the main window
 class MainWindow(QtWidgets.QMainWindow):
